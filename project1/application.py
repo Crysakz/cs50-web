66bfa2551c555643c3e94c26a47a4fcdcdd19302
Tomas Veprek <crysak34@gmail.com>
2019-02-28 20:29:21 +0100

    Added default logic for registration user

--- project1/application.py
+++ project1/application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -20,7 +20,39 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+def validate_register(username, password, password2):
+    """ Function to validate Registration form on the server side """
+    if (not username or not password) or not password2:
+        return False
+    elif password != password2:
+        return False 
+    return True
+
+""" TODO Error Hanlder for custom error, like not same passwords or not filled input fields eg... """
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    """ Landing Page """ 
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """ Register User """ 
+    if request.method == "POST":
+        if validate_register(request.form["username"], request.form["password"], request.form["password2"]):
+            print("succes") #TODO
+        else:
+            return render_template("register.html", alert="Please fill all fields")
+        return render_template("index.html")
+    else:
+        return render_template("register.html")
+    
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """ Login  User """ 
+    if request.method == "POST":
+        return render_template("index.html")
+    else:
+        return render_template("login.html")
