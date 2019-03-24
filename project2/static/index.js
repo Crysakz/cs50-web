458156f143a2d8105df3d2365617d950a459961f
Tomas Veprek <crysak34@gmail.com>
2019-03-24 17:17:30 +0100

    Joining functionality, lint fixes

--- project2/static/index.js
+++ project2/static/index.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-restricted-globals */
-if (!localStorage.getItem('username')) {
+if (!window.localStorage.getItem('username')) {
   // Redirect user if no username
   if (window.location.pathname !== '/') window.location.pathname = '/';
 
@@ -10,39 +9,66 @@ if (!localStorage.getItem('username')) {
       document.querySelector('#submit-button').disabled = false;
     }
   };
-
+  // Store username in user local storage
   document.querySelector('#form').onsubmit = () => {
     const user = document.querySelector('#username').value;
-    localStorage.setItem('username', user);
+    window.localStorage.setItem('username', user);
   };
 } else if (window.location.pathname !== '/chat') {
   window.location.pathname = '/chat';
 }
 document.addEventListener('DOMContentLoaded', () => {
-  // eslint-disable-next-line no-undef
   const socket = io.connect(
-    `${location.protocol}//${document.domain}:${location.port}`,
+    `${window.location.protocol}//${document.domain}:${window.location.port}`,
   );
 
   socket.on('connect', () => {
     document.querySelector('#roomForm').onsubmit = () => {
+      // Send room name to server
       const text = document.querySelector('#room-name').value;
       socket.emit('submit room', { roomName: text });
-      document.querySelector('#room-name').value = '';
+      text.value = '';
       return false;
     };
+
+    document.querySelector('#chatForm').onsubmit = () => {
+      // Send room name to server
+      const message = document.querySelector('#message').value;
+      message.value = '';
+      return false;
+    };
+
+    const list = document.querySelector('#rooms');
+
+    /* Delegate event listener, applies click event to all childs.
+       Even new ones emmited from other users */
+    list.addEventListener('click', (event) => {
+      // Connect user to chat room and leave old on
+      const room = event.target.innerHTML;
+      const username = window.localStorage.getItem('username');
+      socket.emit('join', { room, username });
+    });
   });
 
   socket.on('add room', (data) => {
+    // Apend room name to list of rooms
     const li = document.createElement('li');
     li.innerHTML = data;
     document.querySelector('#rooms').append(li);
   });
 
+  socket.on('joined', (username, room) => {
+    // Send message that user has joned specific room, to all users in the room
+    const p = document.createElement('p');
+    p.innerHTML = `user ${username} joined ${room}`;
+    document.querySelector('#chatForm').append(p);
+  });
+
   socket.on('room already exist', (data) => {
+    // Show user alert if he tries to add existing room
     const errorAlert = document.createElement('div');
-    errorAlert.innerHTML = `Room already exists!${data}`;
+    errorAlert.innerHTML = `${data} Room already exists!$`;
     errorAlert.classList.add('alert', 'alert-danger');
-    document.querySelector('body').append(errorAlert);
+    document.querySelector('body').prepend(errorAlert);
   });
 });
