b48b61c43e9d6cb9b2cc6b02c0ed6f8444ff6ba0
Tomas Veprek <crysak34@gmail.com>
2019-04-14 10:05:27 +0200

    Added methods for list of users

--- project2/rooms.py
+++ project2/rooms.py
@@ -1,8 +1,10 @@
 class Rooms():
     """Initialialize room, which stores 100 msg"""
+
     def __init__(self, name):
         self.name = name
         self.messages = []
+        self.users = []
 
     def append_message(self, list):
         self.messages.append(list)
@@ -11,3 +13,9 @@ class Rooms():
     def check_maximum_lenght(self):
         if len(self.messages) > 100:
             self.messages.pop(0)
+
+    def append_user(self, username):
+        self.users.append(username)
+
+    def remove_user(self, username):
+        return True if self.users.remove(username) else False
